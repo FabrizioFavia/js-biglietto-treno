967ed6f8abe57ee6bab72d53fce2005c66b4922f
FabrizioFavia <personal.favia@gmail.com>
2023-04-14 00:44:48 +0200

    fix(js): cleaned up code

--- js/main.js
+++ js/main.js
@@ -2,24 +2,21 @@ var kmPrice = 0.21;
 var totalPrice = null;
 
 function givePrice() {
-
     var age = document.getElementById("age").value;
     age = Number(age);
     var distance = document.getElementById("distance").value;
     distance = Number(distance);
-    totalPrice = (Number(distance) * kmPrice).toFixed(2);
+    totalPrice = (distance * kmPrice).toFixed(2);
     if (age < 18) {
         var underDiscount = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
         document.getElementById("price").innerText = (`Prezzo del biglietto: € ${underDiscount}`);
     }
     else if (age > 65) {
-        var overDiscount = (totalPrice - (totalPrice * 40 / 100));
-        overDiscount = overDiscount.toFixed(2);
+        var overDiscount = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
         document.getElementById("price").innerText = (`Prezzo del biglietto: € ${overDiscount}`);
     }
     else {
         document.getElementById("price").innerText = (`Prezzo del biglietto: € ${totalPrice}`);
-
     }
     event.preventDefault()
 }
