94cd5e9d1b340d3c3a4e4e8aacb22f1592fef163
FabrizioFavia <personal.favia@gmail.com>
2023-04-13 16:49:33 +0200

    fix(js): fixed file's indentation

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 var kmPrice = 0.21;
 var totalPrice = null;
 
-function givePrice(){
-    
+function givePrice() {
+
     var age = document.getElementById("age").value;
     age = Number(age);
     var distance = document.getElementById("distance").value;
     age = Number(distance);
     totalPrice = (Number(distance) * kmPrice)
-totalPrice = totalPrice.toFixed(2);
-if (age < 18) {
-    var underDiscount = (totalPrice - (totalPrice * 20 / 100));
-    underDiscount = underDiscount.toFixed(2);
-    document.getElementById("price").innerText = (`Prezzo del biglietto: €${underDiscount}`);
-}
-else if (age > 65) {
-    var overDiscount = (totalPrice - (totalPrice * 40 / 100));
-    overDiscount = overDiscount.toFixed(2);
-    document.getElementById("price").innerText = (`Prezzo del biglietto: €${overDiscount}`);
-}
-else{
-    document.getElementById("price").innerText = (`Prezzo del biglietto: €${totalPrice}`);
-}
-event.preventDefault()
+    totalPrice = totalPrice.toFixed(2);
+    if (age < 18) {
+        var underDiscount = (totalPrice - (totalPrice * 20 / 100));
+        underDiscount = underDiscount.toFixed(2);
+        document.getElementById("price").innerText = (`Prezzo del biglietto: € ${underDiscount}`);
+    }
+    else if (age > 65) {
+        var overDiscount = (totalPrice - (totalPrice * 40 / 100));
+        overDiscount = overDiscount.toFixed(2);
+        document.getElementById("price").innerText = (`Prezzo del biglietto: € ${overDiscount}`);
+    }
+    else {
+        document.getElementById("price").innerText = (`Prezzo del biglietto: € ${totalPrice}`);
+    }
+    event.preventDefault()
 }
 
 
